7a2b7ea6f2d583a9c3e6c92bc6deda22add70bc6
haro124 <haroldcarrono1@gmail.com>
2025-02-07 15:33:52 -0500

    main

---
+++ src/main/java/ec/edu/espoch/proyectonuevo/tester/ControladorFalso.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.proyectonuevo.tester;
+
+/**
+ *
+ * @author SO-LAB1-PC14
+ */
+public class ControladorFalso {
+    
+}
