c79677610dec0ecd1e7271c0095aab722de3bb32
haro124 <haroldcarrono1@gmail.com>
2025-02-07 15:30:33 -0500

    main

---
+++ src/main/java/ec/edu/espoch/proyectonuevo/ProyectoNuevo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.proyectonuevo;
+
+/**
+ *
+ * @author SO-LAB1-PC14
+ */
+public class ProyectoNuevo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
