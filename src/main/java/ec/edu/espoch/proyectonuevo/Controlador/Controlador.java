6ab4847cbe50b6a302b3311ac583fc2a9826d292
Marjorie06 <marjoriechal06@gmail.com>
2025-02-13 10:20:03 -0500

    Subir

---
+++ src/main/java/ec/edu/espoch/proyectonuevo/Controlador/Controlador.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.proyectonuevo.Controlador;
+
+import ec.edu.espoch.proyectonuevo.vista.Principal;
+
+
+
+/**
+ *
+ * @author TAMMY
+ */
+public class Controlador {
+    private Principal principal;
+    private TesterControlador gestorTarea;
+    //Cuando hagas conexion con el modelo --> quita los /// de la linea 17
+    //private GestorTarea gestorTarea;
+
+    public Controlador(Principal proyectonuevo) {
+        this.principal = proyectonuevo;
+         //Cambia el constructor de la clase modelo
+        this.gestorTarea = new TesterControlador();
+    }
+ 
+
+    public void agregarTarea() {
+
+        try {
+            if (this.principal != null) {
+                Tarea objTarea = new Tarea();
+                objTarea.setTitulo(this.principal.getTitulo());
+                objTarea.setDescripcion(this.principal.getDescripcion());
+                objTarea.setEstado(this.principal.getEstado());
+                gestorTarea.prueba(objTarea);
+
+            } else {
+                principal.error("Completa los datos!");
+            }
+        } catch (Exception e) {
+            principal.error("Consulta al ing Pedro");
+        }
+    }
+}
+
